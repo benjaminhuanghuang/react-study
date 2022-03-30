29c1610f06aab4b581733f93b47453180338db20
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-29 23:00:51 -0700

    update

---
+++ react-material-ui-router/README.md
@@ -0,0 +1,15 @@
+# React + MaterialUI + Router
+
+
+## setup
+```
+  npx create-react-app react-material-ui-router
+
+  npm i @material-ui/core @material-ui/icons react-router-dom
+```
+
+## Left bar: Drawer
+
+
+
+
---
+++ react-material-ui-router/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// @material-ui
+import { makeStyles } from "@material-ui/core/styles";
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+
+import HomeIcon from "@material-ui/icons/Home";
+import InfoIcon from "@material-ui/icons/Info";
+//
+import Home from "./Home";
+import About from "./About";
+
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: "inherit" },
+  link: {
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Router>
+      <div style={{ display: "flex" }}>
+        <Drawer
+          style={{ width: "220px" }}
+          variant="persistent"
+          anchor="left"
+          open={true}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Home"} />
+              </ListItem>
+            </Link>
+            <Link to="/about" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary={"About"} />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
