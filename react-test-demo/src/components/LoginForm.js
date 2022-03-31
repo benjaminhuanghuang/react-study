c588ea931fb54ef05d93251f436ce454dd150d6c
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-30 20:41:45 -0700

    ut

--- react-test-demo/src/components/LoginForm.js
+++ react-test-demo/src/components/LoginForm.js
@@ -1,22 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import validator from "validator";
+
 
 export default function LoginForm() {
+  const [signupInput, setSignupInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    setSignupInput({
+      ...signupInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (e) =>{
+    e.preventDefault();
+    //
+    if(!validator.isEmail(signupInput.email)){
+      setError('The email you input is invalid.');
+    }
+  }
+
   return (
-    <div className='container my-5'>
-      <form action="">
+    <div className="container my-5">
+      <form>
         <div className="mb-3">
-          <label htmlFor="email" className='form-label'>Email</label>
-          <input type="email" id="email" name="email" className='form-control' />
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={signupInput.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className='form-label'>Password</label>
-          <input type="password" id="password" name="password" className='form-control' />
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+            value={signupInput.password}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="confirm-password" className='form-label'>Confirm Password</label>
-          <input type="password" id="confirm-password" name="confirm-password" className='form-control' />
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            className="form-control"
+            value={signupInput.confirmPassword}
+            onChange={handleChange}
+          />
         </div>
+        {
+          error && <p className="text-danger">{error}</p>
+        }
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
       </form>
     </div>
-  )
+  );
 }
--- react-test-demo/src/components/LoginForm.spec.js
+++ react-test-demo/src/components/LoginForm.spec.js
@@ -1,16 +1,69 @@
-import { render, screen } from '@testing-library/react';
-import LoginFrom from './LoginForm';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+//
+import LoginFrom from "./LoginForm";
 
-test('Inputs should be initially empty', () => {
+test("Inputs should be initially empty", () => {
   render(<LoginFrom />);
 
-  const emailInputElement = screen.getByRole('textbox');
+  const emailInputElement = screen.getByRole("textbox");
   expect(emailInputElement.value).toBe("");
-
 
   const passwordInputElement = screen.getByLabelText(/^password/i);
   expect(passwordInputElement.value).toBe("");
 
   const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
-  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("Should be able to type an email", () => {
+  render(<LoginFrom />);
+
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const email = "aaa@bbb.com";
+  userEvent.type(emailInputElement, email);
+  expect(emailInputElement.value).toBe(email);
+});
+
+test("Should be able to type a password", () => {
+  render(<LoginFrom />);
+
+  const passwordInputElement = screen.getByLabelText(/^password/i);
+  const pwd = "password!";
+  userEvent.type(passwordInputElement, pwd);
+  expect(passwordInputElement.value).toBe(pwd);
+});
+
+test("Should be able to type a confirm password", () => {
+  render(<LoginFrom />);
+
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
+  const pwd = "password!";
+  userEvent.type(confirmPasswordInputElement, pwd);
+  expect(confirmPasswordInputElement.value).toBe(pwd);
+});
+
+test("Should show email error message on invalid email", () => {
+  render(<LoginFrom />);
+
+  const errorMsg = /The email you input is invalid./i;
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const email = "aaa.com";
+
+  const submitButtonElement = screen.getByRole("button", {
+    name: /submit/i,
+  });
+  // getByText will throw if the element does't exist
+  let  emailErrorElement = screen.queryByText(errorMsg);
+  expect(emailErrorElement).not.toBeInTheDocument();
+
+  userEvent.type(emailInputElement, email);
+  userEvent.click(submitButtonElement);
+
+  emailErrorElement = screen.queryByText(errorMsg);
+  expect(emailErrorElement).toBeInTheDocument();
 });
