2802f96d031cae8d951b7f808ee7018d4e79a2d6
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-08-21 12:13:43 -0700

    update

--- _courses/Advanced React/react-design-patterns/src/App.jsx
+++ _courses/Advanced React/react-design-patterns/src/App.jsx
@@ -1,6 +1,15 @@
 
-import { SplitScreen } from './components/split-screen'
 
+import { SplitScreen } from './components/split-screen'
+//-- Lists
+import { RegularList } from './components/lists/Regular';
+import { authors } from './data/authors'
+import { books } from './data/books'
+import { SmallAuthorListItem } from './components/authors/SmallListItems';
+import { LargeAuthorListItem } from './components/authors/LargeListItems';
+import { LargeBookListItem } from './components/books/LargeListItems';
+//-- Modal
+import { Modal } from './components/modal';
 
 const LeftSideComp = ({ title }) => {
   return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
@@ -11,10 +20,21 @@ const RightSideComp = ({ title }) => {
 };
 function App() {
   return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComp title={'Right'} />
-      <RightSideComp title={'Left'} />
-    </SplitScreen>
+    <>
+      <SplitScreen leftWidth={1} rightWidth={3}>
+        <LeftSideComp title={'Right'} />
+        <RightSideComp title={'Left'} />
+      </SplitScreen>
+
+      <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem}></RegularList>
+      <RegularList items={authors} sourceName={"author"} ItemComponent={LargeAuthorListItem}></RegularList>
+
+      <RegularList items={books} sourceName={"book"} ItemComponent={LargeBookListItem}></RegularList>
+     
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
+    </>
   )
 }
 
