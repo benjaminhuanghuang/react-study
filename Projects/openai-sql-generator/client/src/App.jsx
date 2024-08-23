5f72c447773af7e06891988021af013b67154f6c
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-08-22 22:45:37 -0700

    update

--- Projects/openai-sql-generator/client/src/App.jsx
+++ Projects/openai-sql-generator/client/src/App.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+
 import styles from './index.module.css'
 import sqlServer from "./assets/sql-server.png";
 
 function App() {
+  const [userPrompt, setUserPrompt] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // const query = await generateQuery();
+    // setSqlQuery(query);
+  };
+
+  const generateQuery = async () => {
+    // const response = await fetch("http://localhost:3002/generate", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ queryDescription: userPrompt }),
+    // });
+
+    // const data = await response.json();
+    // return data.sqlQuery.trim();
+  };
+
+
 
   return (
     <main className={styles.main}>
 
       <img src={sqlServer} className={styles.icon} alt="SQL server" />
       <h3>Generate SQL</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          value={userPrompt}
+          onChange={(e) => setUserPrompt(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+      <pre>{sqlQuery}</pre>
     </main>
   )
 }
