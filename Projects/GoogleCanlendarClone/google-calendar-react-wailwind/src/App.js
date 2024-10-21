d25a6e560c646de293823b6d1b76964c8f5498df
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-10-20 21:34:41 -0700

    update

--- Projects/GoogleCanlendarClone/google-calendar-react-wailwind/src/App.js
+++ Projects/GoogleCanlendarClone/google-calendar-react-wailwind/src/App.js
@@ -1,9 +1,16 @@
+import React, { useState, useContext, useEffect } from "react";
+
 import './App.css';
+import { getMonth } from "./util";
 import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
+import EventModal from "./components/EventModal";
 
 function App() {
+  const [currenMonth, setCurrentMonth] = useState(getMonth());
+
   return (
     <>
         <div className="h-screen flex flex-col">
