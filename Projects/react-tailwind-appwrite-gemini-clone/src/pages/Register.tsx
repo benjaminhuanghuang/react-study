a189fa47c4f3636e7175a50f50348ea83a3a478a
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-11-28 00:34:12 -0800

    update

--- Projects/react-tailwind-appwrite-gemini-clone/src/pages/Register.tsx
+++ Projects/react-tailwind-appwrite-gemini-clone/src/pages/Register.tsx
@@ -5,38 +5,42 @@ import Button from '../components/Button';
 //
 import { logoDark, logoLight, banner } from '../assets/assets';
 
-
 function Register() {
   return (
     <>
       <PageTitle title='Create an account' />
-      <div className='bg-black relative w-screen h-dvh p-2 grid grid-cols-2 lg:grid-cols[1fr, 1.2fr] lg:gap-2'>
-        <div className=''>
-          <Link to='/'>
+      <div className='relative w-screen h-dvh p-2 grid grid-cols-1 lg:grid-cols-[1fr,1.2fr] lg:gap-2'>
+        <div className='flex flex-col p-4'>
+          <Link
+            to='/'
+            className='max-w-max mb-auto mx-auto lg:mx-0'
+          >
             <img
               src={logoLight}
               alt='phoenix logo'
               width={133}
               height={24}
-              className=''
+              className='dark:hidden'
             />
             <img
               src={logoDark}
               alt='phoenix logo'
               width={133}
               height={24}
-              className=''
+              className='hidden dark:block'
             />
           </Link>
-          <div className=''>
-            <h2 className=''>Create an account</h2>
-            <p className=''>
+          <div className='flex flex-col gap-2 max-w-[480px] w-full mx-auto'>
+            <h2 className='text-displaySmall font-semibold text-light-onBackground dark:text-dark-onBackground text-center'>
+              Create an account
+            </h2>
+            <p className='text-bodyLarge text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant mt-1 mb-5 text-center px-2'>
               Register today and gain access to powerful tools that will
               supercharge your ideas.
             </p>
             <Form
               method='POST'
-              className=''
+              className='grid grid-cols-1 gap-4'
             >
               <TextField
                 type='text'
@@ -46,15 +50,16 @@ function Register() {
                 required={true}
                 autoFocus={true}
                 classes='border'
+                helperText='Please enter your full name'
               />
-               <TextField
+              <TextField
                 type='email'
                 name='email'
                 label='Email'
                 placeholder='Email'
                 required={true}
               />
-               <TextField
+              <TextField
                 type='password'
                 name='password'
                 label='Password'
@@ -64,20 +69,32 @@ function Register() {
               <Button type='submit'> Create account </Button>
             </Form>
 
-            <p className="">
+            <p className='text-bodyMedium text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant text-center mt-4'>
               Already have an account?
-              <Link to='/login' className=''>
+              <Link
+                to='/login'
+                className='link inline-block ms-1 text-light-onSurface dark:text-dark-onSurface'
+              >
                 Sign In
-                </Link>
+              </Link>
             </p>
           </div>
-          <p className=''>
+          <p className='mt-auto mx-auto text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant text-bodyMedium lg:mx-0'>
             &copy; 2021 Phoenix. All rights reserved.
           </p>
         </div>
-        <div className=''>
-          <img src={banner} alt="" className='img-cover'/>
-          <p className=""> Chat with Phoenix to supercharge your ideas.</p>
+        <div className='hidden img-box lg:block lg:relative lg:rounded-large'>
+          <img
+            src={banner}
+            alt=''
+            className='img-cover'
+          />
+          <p
+            className='absolute bottom-10 left-12 right-12 z-10 text-displayLarge font-semibold leading-tight text-right text-light-onSurface dark:text-dark-onSurface dark-shadow-sm
+          2xl:text-[72px]'
+          >
+            Chat with Phoenix to supercharge your ideas.
+          </p>
         </div>
       </div>
     </>
