ab865c3c898ed746c3daef457d27e9138454fc7c
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-02 23:46:27 -0800

    update

--- _React-Deep-Dive/react18-code-source/src/main.tsx
+++ _React-Deep-Dive/react18-code-source/src/main.tsx
@@ -1,8 +1,9 @@
 import {jsx} from './react/jsx';
-
-
+import { ReactElementType } from '@/shared/ReactTypes';
+import { useState } from '@/react';
 
 const App: any = () => {    
+    const [num, setNum] = useState(2);
     return (
         <h1>
             <h2>
